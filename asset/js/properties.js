dcf46b3affcc78d6ba8b1b59dd8cbdd42398d349
christopherreynard1608-boop <christopher.reynard1608@gmail.com>
2026-03-12 15:03:33 +0700

    Update properties.js

--- asset/js/properties.js
+++ asset/js/properties.js
@@ -34,7 +34,7 @@ function openGallery(setName) {
         if (src.toLowerCase().includes('yout')) {
             const videoId = getYouTubeId(src);
           
-            const thumbUrl = 'https://img.youtube.com/vi/${videoId}/hqdefault.jpg';
+            const thumbUrl = 'https://img.youtube.com/vi/${videoId}/maxresdefault.jpg';
 
             content = `
                 <div class="slide fade">
@@ -152,4 +152,5 @@ function startLoading() {
 
 
 window.onload = startLoading;
+
 
